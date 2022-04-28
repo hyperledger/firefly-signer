f7b9da34f83f559768a67abce1d8af84ca5388cd
Peter Broadhurst <peter.broadhurst@kaleido.io>
2022-04-28 16:17:48 -0400

    RLP decoding implementation
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- pkg/rlp/rlp.go
+++ pkg/rlp/rlp.go
@@ -16,42 +16,10 @@
 
 package rlp
 
-import "math/big"
-
-type rlpOffset byte
-
-const (
-	/**
-	 * [0x80] If a string is 0-55 bytes long, the RLP encoding consists of a single byte with value
-	 * 0x80 plus the length of the string followed by the string. The range of the first byte is
-	 * thus [0x80, 0xb7].
-	 */
-	shortString rlpOffset = 0x80
-
-	/**
-	 * [0xb7] If a string is more than 55 bytes long, the RLP encoding consists of a single byte
-	 * with value 0xb7 plus the length of the length of the string in binary form, followed by the
-	 * length of the string, followed by the string. For example, a length-1024 string would be
-	 * encoded as \xb9\x04\x00 followed by the string. The range of the first byte is thus [0xb8,
-	 * 0xbf].
-	 */
-	// longString rlpOffset = 0xb7
-
-	/**
-	 * [0xc0] If the total payload of a list (i.e. the combined length of all its items) is 0-55
-	 * bytes long, the RLP encoding consists of a single byte with value 0xc0 plus the length of the
-	 * list followed by the concatenation of the RLP encodings of the items. The range of the first
-	 * byte is thus [0xc0, 0xf7].
-	 */
-	shortList rlpOffset = 0xc0
-
-	/**
-	 * [0xf7] If the total payload of a list is more than 55 bytes long, the RLP encoding consists
-	 * of a single byte with value 0xf7 plus the length of the length of the list in binary form,
-	 * followed by the length of the list, followed by the concatenation of the RLP encodings of the
-	 * items. The range of the first byte is thus [0xf8, 0xff].
-	 */
-	// longList rlpOffset = 0xf7
+import (
+	"encoding/hex"
+	"math/big"
+	"strings"
 )
 
 type Data []byte
@@ -60,7 +28,6 @@ type List []Element
 
 type Element interface {
 	IsList() bool
-	List() List
 	Encode() []byte
 }
 
@@ -72,6 +39,22 @@ func WrapInt(i *big.Int) Data {
 	return Data(i.Bytes())
 }
 
+func WrapHex(s string) (Data, error) {
+	b, err := hex.DecodeString(strings.TrimPrefix(s, "0x"))
+	if err != nil {
+		return nil, err
+	}
+	return Data(b), nil
+}
+
+func MustWrapHex(s string) Data {
+	b, err := WrapHex(s)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
 func (r Data) Int() *big.Int {
 	if r == nil {
 		return nil
@@ -81,11 +64,7 @@ func (r Data) Int() *big.Int {
 }
 
 func (r Data) Encode() []byte {
-	return encodeBytes(r, shortString)
-}
-
-func (r Data) List() List {
-	return nil
+	return encodeBytes(r, false)
 }
 
 func (r Data) IsList() bool {
@@ -94,18 +73,14 @@ func (r Data) IsList() bool {
 
 func (l List) Encode() []byte {
 	if len(l) == 0 {
-		return encodeBytes([]byte{}, shortList)
+		return encodeBytes([]byte{}, true)
 	}
 	var concatenation []byte
 	for _, entry := range l {
 		concatenation = append(concatenation, entry.Encode()...)
 	}
-	return encodeBytes(concatenation, shortList)
+	return encodeBytes(concatenation, true)
 
-}
-
-func (l List) List() List {
-	return l
 }
 
 func (l List) IsList() bool {
