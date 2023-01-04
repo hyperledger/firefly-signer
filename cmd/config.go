e8a662fac56e24e17eaa44505137b514190d5574
Jim Zhang <jim.zhang@kaleido.io>
2023-01-04 11:44:17 -0500

    Fix formatting of the config ref docs header to match expected content
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- cmd/config.go
+++ cmd/config.go
@@ -35,12 +35,13 @@ nav_order: 2
 {: .no_toc }
 
 <!-- ## Table of contents
-
 {: .no_toc .text-delta }
+
 1. TOC
 {:toc} -->
 
----`
+---
+`
 
 func configCommand() *cobra.Command {
 	versionCmd := &cobra.Command{
